e7b2d3ecaa25dc86e5fc40c5b024ae9a1d563d20
deepakmaur <120335053+deepakmaur@users.noreply.github.com>
2023-09-29 09:38:13 +0530

    Create Monotonic_array.cpp

---
+++ Sept/Monotonic_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool dec=true, inc=true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                dec=false;
+            }
+            else if(nums[i]<nums[i-1]){
+                inc=false;
+            }
+            if (!inc && !dec) {
+                return false;
+            }
+        }
+        return true;
+        }
+
+        
+};
